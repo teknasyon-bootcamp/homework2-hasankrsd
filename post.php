e4e2c299dc6ac6c0b503cd244461a5082da15ccd
hasankrsd <73365934+hasankrsd@users.noreply.github.com>
2021-08-28 00:42:58 +0300

    Update post.php

--- post.php
+++ post.php
@@ -23,3 +23,50 @@
  * - `getPostDetails` fonksiyonu tetiklenerek ilgili içeriğin çıktısı gösterilmeli.
  */
 
+ // Önce function.php dosyasını dahil ediyoruz.
+require_once("functions.php");
+
+//id değeri eğer boş gelirse default değer 1 olacak
+if (!isset($id)) {
+    $id=1;
+}
+
+//Titile değeri var mı yok mu bakılır yoksa default değer atanır yoksa olan değer düşer
+if(!isset($value["title"]))
+ 	{
+        $title="Hasan Kürşad KORKMAZ Bootcamp 2. Hafta Ödev";
+    }
+ 	else
+ 	{
+ 		$title=$value["title"];
+ 	}
+
+//Type içim hergangi bir değer düştüyse o işlenir düşmediyse default olarak normal olur
+if(!isset($value["type"]))
+ 	{
+ 		$type="normal";
+ 	}
+ 	else
+ 	{
+ 		$type=$value["type"];
+ 	}
+
+     //Type'ın değerine göre arkaplan rengi değişir
+     if ($type=="normal") {
+         $background="none";
+     }
+     elseif ($type=="urgent") {
+         $background="red";
+     }
+     elseif ($type=="warning") {
+        $background="yellow";
+     }
+     //type değerine göre oluşan background -color ile div oluşturulur.
+     echo "<div style='background-color:".$background.";'>";
+
+     //ilgili fonksiyondan çağıralan değerlere göre fonksiyon çalışır.
+     getPostDetails($id,$title);
+     echo "</div>";
+
+
+     
